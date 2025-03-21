c913ccf9189dff14ff49811c79783b73f54d896b
Jason-ine <jdpivaral@ine.gob.gt>
2025-03-21 15:28:10 -0600

    Agrego cambios para cargar con store produceres y cargo mejor da data desde excel

--- src/main/java/ExcelToDatabase.java
+++ src/main/java/ExcelToDatabase.java
@@ -16,9 +16,9 @@ import java.util.Map;
 
 public class ExcelToDatabase {
 
-    // Método principal
+    
     public static void main(String[] args) {
-        // Procesar cada archivo Excel y tabla correspondiente
+        
         procesarArchivoExcel(
             "C:\\Users\\jdpivaral\\WebScraping\\solucion\\archivos_excel\\Base_IPM.xlsx",
             "SIP_IPM"
@@ -45,22 +45,22 @@ public class ExcelToDatabase {
         );
     }
 
-    // Método para procesar un archivo Excel y cargar los datos en la tabla correspondiente
+    
     public static void procesarArchivoExcel(String rutaExcel, String nombreTabla) {
         Connection conexion = null;
         try {
-            // 1. Conectar a la base de datos (SQL Server)
-            String url = "jdbc:sqlserver://0057A31D:1433;databaseName=prod;encrypt=true;trustServerCertificate=true";
-            String usu = "sa";
-            String contraseña = "Abc$2020";
+          
+            String url = "jdbc:sqlserver://10.0.0.19:1433;databaseName=TablerosIPC;encrypt=true;trustServerCertificate=true";
+            String usu = "ipcwork";
+            String contraseña = "1pcWR1t3R/*2025";
             conexion = DriverManager.getConnection(url, usu, contraseña);
 
-            // 2. Leer el archivo Excel
+            // Leer el archivo Excel
             InputStream archivoExcel = new FileInputStream(rutaExcel);
             Workbook workbook = new XSSFWorkbook(archivoExcel);
             Sheet hoja = workbook.getSheetAt(0); // Lee la primera hoja
 
-            // 3. Leer la primera fila (encabezados) para obtener los índices de las columnas
+            // Lee la primera fila (encabezados) para obtener los índices de las columnas
             Row encabezados = hoja.getRow(0);
             Map<String, Integer> columnas = new HashMap<>();
 
@@ -68,11 +68,10 @@ public class ExcelToDatabase {
                 columnas.put(celda.getStringCellValue().trim().toLowerCase(), celda.getColumnIndex());
             }
 
-            // 4. Preparar la consulta SQL para insertar datos
+            // Preparar la consulta SQL para insertar datos
             String sql = "";
             PreparedStatement pstmt = null;
 
-            // 5. Determinar la estructura de la tabla y preparar la consulta SQL
             switch (nombreTabla) {
                 case "SIP_IPM":
                     sql = "INSERT INTO SIP_IPM (region, departamento, municipio, semana, usuario, numero_boleta, codigo_tipo_fuente, tipo_fuente_nombre, codigo_fuente, nombre_fuente, direccion, zona, latitud, longitud, georefenciada, id, correlativo, fecha, mes, anio) " +
@@ -110,7 +109,7 @@ public class ExcelToDatabase {
                     break;
             }
 
-            // 6. Cerrar recursos
+            
             workbook.close();
             archivoExcel.close();
             pstmt.close();
@@ -119,7 +118,7 @@ public class ExcelToDatabase {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // Cerrar la conexión a la base de datos
+            
             if (conexion != null) {
                 try {
                     conexion.close();
@@ -130,7 +129,7 @@ public class ExcelToDatabase {
         }
     }
 
-    // Método para procesar datos de la tabla SIP_IPM
+    
     private static void procesarSIP_IPM(Sheet hoja, PreparedStatement pstmt, Map<String, Integer> columnas) throws Exception {
 
         String region;
@@ -161,7 +160,7 @@ public class ExcelToDatabase {
                 continue;
             }
     
-            // Obtener los valores de cada celda usando los nombres de las columnas
+           
             region = obtenerValorCelda(fila.getCell(columnas.get("region")));
             departamento = obtenerValorCelda(fila.getCell(columnas.get("departamento")));
             municipio = obtenerValorCelda(fila.getCell(columnas.get("municipio")));
@@ -170,12 +169,12 @@ public class ExcelToDatabase {
             numeroBoleta = obtenerValorNumerico(fila.getCell(columnas.get("numero_boleta")));
             codigoTipoFuente = obtenerValorCelda(fila.getCell(columnas.get("codigo_tipo_fuente")));
             tipoFuenteNombre = obtenerValorCelda(fila.getCell(columnas.get("tipo_fuente_nombre")));
-            codigoFuente = obtenerValorNumerico(fila.getCell(columnas.get("codigo_fuente"))); // Obtener como Double
+            codigoFuente = obtenerValorNumerico(fila.getCell(columnas.get("codigo_fuente")));
             nombreFuente = obtenerValorCelda(fila.getCell(columnas.get("nombre_fuente")));
             direccion = obtenerValorCelda(fila.getCell(columnas.get("direccion")));
             zona = obtenerValorNumerico(fila.getCell(columnas.get("zona")));
-            latitud = obtenerValorNumerico(fila.getCell(columnas.get("latitud"))); // Leer como double
-            longitud = obtenerValorNumerico(fila.getCell(columnas.get("longitud"))); // Leer como double
+            latitud = obtenerValorNumerico(fila.getCell(columnas.get("latitud"))); 
+            longitud = obtenerValorNumerico(fila.getCell(columnas.get("longitud"))); 
             georefenciada = obtenerValorNumerico(fila.getCell(columnas.get("georefenciada")));
             id = obtenerValorNumerico(fila.getCell(columnas.get("id")));
             correlativo = obtenerValorNumerico(fila.getCell(columnas.get("correlativo")));
@@ -195,7 +194,7 @@ public class ExcelToDatabase {
             latitudStr = (latitud != null) ? String.valueOf(latitud) : null;
             longitudStr = (longitud != null) ? String.valueOf(longitud) : null;
     
-            // Asignar los valores a la consulta SQL
+            
             pstmt.setString(1, region);
             pstmt.setString(2, departamento);
             pstmt.setString(3, municipio);
@@ -213,7 +212,7 @@ public class ExcelToDatabase {
             pstmt.setString(7, codigoTipoFuente);
             pstmt.setString(8, tipoFuenteNombre);
             if (codigoFuente != null) {
-                pstmt.setInt(9, codigoFuente.intValue()); // Convertir a int
+                pstmt.setInt(9, codigoFuente.intValue()); 
             } else {
                 pstmt.setNull(9, java.sql.Types.INTEGER);
             }
@@ -224,8 +223,8 @@ public class ExcelToDatabase {
             } else {
                 pstmt.setNull(12, java.sql.Types.INTEGER);
             }
-            pstmt.setString(13, latitudStr); // Insertar como String
-            pstmt.setString(14, longitudStr); // Insertar como String
+            pstmt.setString(13, latitudStr); 
+            pstmt.setString(14, longitudStr); 
             if (georefenciada != null) {
                 pstmt.setInt(15, georefenciada.intValue());
             } else {
@@ -253,12 +252,12 @@ public class ExcelToDatabase {
                 pstmt.setNull(20, java.sql.Types.INTEGER);
             }
     
-            // Ejecutar la inserción
+            
             pstmt.executeUpdate();
         }
     }
 
-    // Método para procesar datos de la tabla SIP_IPP
+    
     private static void procesarSIP_IPP(Sheet hoja, PreparedStatement pstmt, Map<String, Integer> columnas) throws Exception {
         Double numero;
         String estado;
@@ -288,14 +287,13 @@ public class ExcelToDatabase {
                 continue;
             }
     
-            // Obtener los valores de cada celda usando los nombres de las columnas
             numero = obtenerValorNumerico(fila.getCell(columnas.get("numero")));
             estado = obtenerValorCelda(fila.getCell(columnas.get("estado")));
             empadronada = obtenerValorCelda(fila.getCell(columnas.get("empadronada")));
             tipoEmpresa = obtenerValorCelda(fila.getCell(columnas.get("tipo_empresa")));
             codigoTipologia = obtenerValorNumerico(fila.getCell(columnas.get("codigo_tipologia")));
             tipologiaNombre = obtenerValorCelda(fila.getCell(columnas.get("tipologia_nombre")));
-            nit = obtenerValorCelda(fila.getCell(columnas.get("nit"))); // Obtener el NIT como texto
+            nit = obtenerValorCelda(fila.getCell(columnas.get("nit")));
             ajuste = obtenerValorCelda(fila.getCell(columnas.get("ajuste")));
             razonSocial = obtenerValorCelda(fila.getCell(columnas.get("razon_social")));
             nombreComercial = obtenerValorCelda(fila.getCell(columnas.get("nombre_comercial")));
@@ -303,18 +301,18 @@ public class ExcelToDatabase {
             departamento = obtenerValorCelda(fila.getCell(columnas.get("departamento")));
             municipio = obtenerValorCelda(fila.getCell(columnas.get("municipio")));
             zona = obtenerValorNumerico(fila.getCell(columnas.get("zona")));
-            latitud = obtenerValorNumerico(fila.getCell(columnas.get("latitud"))); // Leer como double
-            longitud = obtenerValorNumerico(fila.getCell(columnas.get("longitud"))); // Leer como double
+            latitud = obtenerValorNumerico(fila.getCell(columnas.get("latitud"))); 
+            longitud = obtenerValorNumerico(fila.getCell(columnas.get("longitud"))); 
             georeferenciada = obtenerValorNumerico(fila.getCell(columnas.get("georeferenciada")));
             telefono = obtenerValorCelda(fila.getCell(columnas.get("telefono")));
             actividadEconomica = obtenerValorCelda(fila.getCell(columnas.get("actividad_economica")));
             ciiu = obtenerValorCelda(fila.getCell(columnas.get("ciiu")));
     
-            // Convertir latitud y longitud a String
+            
             latitudStr = (latitud != null) ? String.valueOf(latitud) : null;
             longitudStr = (longitud != null) ? String.valueOf(longitud) : null;
     
-            // Asignar los valores a la consulta SQL
+           
             if (numero != null) {
                 pstmt.setDouble(1, numero);
             } else {
@@ -341,8 +339,8 @@ public class ExcelToDatabase {
             } else {
                 pstmt.setNull(14, java.sql.Types.BIGINT);
             }
-            pstmt.setString(15, latitudStr); // Insertar como String
-            pstmt.setString(16, longitudStr); // Insertar como String
+            pstmt.setString(15, latitudStr); 
+            pstmt.setString(16, longitudStr); 
             if (georeferenciada != null) {
                 pstmt.setLong(17, georeferenciada.longValue());
             } else {
@@ -352,12 +350,10 @@ public class ExcelToDatabase {
             pstmt.setString(19, actividadEconomica);
             pstmt.setString(20, ciiu);
     
-            // Ejecutar la inserción
             pstmt.executeUpdate();
         }
     }
 
-    // Método para procesar datos de la tabla SIP_Cobertura_Fuentes
     private static void procesarSIP_Cobertura_Fuentes(Sheet hoja, PreparedStatement pstmt, Map<String, Integer> columnas) throws Exception {
         Double regionId;
         String ubicacion;
@@ -369,13 +365,13 @@ public class ExcelToDatabase {
                 continue;
             }
     
-            // Obtener los valores de cada celda usando los nombres de las columnas
+            
             regionId = obtenerValorNumerico(fila.getCell(columnas.get("region_id")));
             ubicacion = obtenerValorCelda(fila.getCell(columnas.get("ubicacion")));
             faltantes = obtenerValorNumerico(fila.getCell(columnas.get("faltantes")));
             departamento = obtenerValorCelda(fila.getCell(columnas.get("departamento")));
     
-            // Asignar los valores a la consulta SQL
+           
             if (regionId != null) {
                 pstmt.setLong(1, regionId.longValue());
             } else {
@@ -389,12 +385,12 @@ public class ExcelToDatabase {
             }
             pstmt.setString(4, departamento);
     
-            // Ejecutar la inserción
+           
             pstmt.executeUpdate();
         }
     }
 
-    // Método para procesar datos de la tabla SIP_IPC_Precios_Promedio
+
     private static void procesarSIP_IPC_Precios_Promedio(Sheet hoja, PreparedStatement pstmt, Map<String, Integer> columnas) throws Exception {
         Double codigoProducto;
         String productoNombre;
@@ -412,7 +408,7 @@ public class ExcelToDatabase {
                 continue;
             }
     
-            // Obtener los valores de cada celda usando los nombres de las columnas
+            
             codigoProducto = obtenerValorNumerico(fila.getCell(columnas.get("cod_prod")));
             productoNombre = obtenerValorCelda(fila.getCell(columnas.get("producto_nombre")));
             codigoVariedad = obtenerValorNumerico(fila.getCell(columnas.get("codigo_articulo")));
@@ -424,7 +420,7 @@ public class ExcelToDatabase {
             anio = obtenerValorNumerico(fila.getCell(columnas.get("anio")));
             mes = obtenerValorNumerico(fila.getCell(columnas.get("mes")));
     
-            // Asignar los valores a la consulta SQL
+            
             if (codigoProducto != null) {
                 pstmt.setLong(1, codigoProducto.longValue());
             } else {
@@ -468,12 +464,12 @@ public class ExcelToDatabase {
                 pstmt.setNull(10, java.sql.Types.INTEGER);
             }
     
-            // Ejecutar la inserción
+           
             pstmt.executeUpdate();
         }
     }
 
-    // Método para procesar datos de la tabla SIP_IPMC
+    
     private static void procesarSIP_IPMC(Sheet hoja, PreparedStatement pstmt, Map<String, Integer> columnas) throws Exception {
         String region;
         String departamento;
@@ -503,7 +499,7 @@ public class ExcelToDatabase {
                 continue;
             }
     
-            // Obtener los valores de cada celda usando los nombres de las columnas
+            
             fechaStr = obtenerValorCelda(fila.getCell(columnas.get("fecha")));
             region = obtenerValorCelda(fila.getCell(columnas.get("region")));
             departamento = obtenerValorCelda(fila.getCell(columnas.get("departamento")));
@@ -517,14 +513,14 @@ public class ExcelToDatabase {
             nombreFuente = obtenerValorCelda(fila.getCell(columnas.get("nombre_fuente")));
             direccion = obtenerValorCelda(fila.getCell(columnas.get("direccion")));
             zona = obtenerValorNumerico(fila.getCell(columnas.get("zona")));
-            latitud = obtenerValorNumerico(fila.getCell(columnas.get("gps_latitud"))); // Leer como double
-            longitud = obtenerValorNumerico(fila.getCell(columnas.get("gps_longitud"))); // Leer como double
+            latitud = obtenerValorNumerico(fila.getCell(columnas.get("gps_latitud"))); 
+            longitud = obtenerValorNumerico(fila.getCell(columnas.get("gps_longitud"))); 
             georefenciada = obtenerValorNumerico(fila.getCell(columnas.get("georeferenciada")));
             id = obtenerValorNumerico(fila.getCell(columnas.get("id")));
             correlativo = obtenerValorNumerico(fila.getCell(columnas.get("correlativo")));
             mes = obtenerValorCelda(fila.getCell(columnas.get("mes")));
     
-            // Convertir la fecha de String a Timestamp
+            
             Timestamp fechaTimestamp = null;
             if (fechaStr != null) {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -532,15 +528,15 @@ public class ExcelToDatabase {
                 fechaTimestamp = new Timestamp(fecha.getTime());
             }
     
-            // Extraer el año de la fecha
+            
             anio = obtenerAnioDesdeFecha(fechaTimestamp != null ? new Date(fechaTimestamp.getTime()) : new Date());
     
-            // Convertir latitud y longitud a String
+           
             latitudStr = (latitud != null) ? String.valueOf(latitud) : null;
             longitudStr = (longitud != null) ? String.valueOf(longitud) : null;
     
     
-            // Asignar los valores a la consulta SQL
+            
             pstmt.setString(1, region);
             pstmt.setString(2, departamento);
             pstmt.setString(3, municipio);
@@ -565,8 +561,8 @@ public class ExcelToDatabase {
             } else {
                 pstmt.setNull(12, java.sql.Types.INTEGER);
             }
-            pstmt.setString(13, latitudStr); // Insertar como String
-            pstmt.setString(14, longitudStr); // Insertar como String
+            pstmt.setString(13, latitudStr); 
+            pstmt.setString(14, longitudStr); 
             if (georefenciada != null) {
                 pstmt.setInt(15, georefenciada.intValue());
             } else {
@@ -590,7 +586,7 @@ public class ExcelToDatabase {
             pstmt.setString(19, mes);
             pstmt.setInt(20, anio);
     
-            // Ejecutar la inserción
+            
             pstmt.executeUpdate();
         }
     }
@@ -602,11 +598,11 @@ public class ExcelToDatabase {
     // Método auxiliar para obtener el valor de una celda como String
     private static String obtenerValorCelda(Cell celda) {
         if (celda == null || celda.getCellType() == CellType.BLANK) {
-            return null; // Devuelve null si la celda está vacía o es nula
+            return null; 
         }
         switch (celda.getCellType()) {
             case STRING:
-                return celda.getStringCellValue().trim(); // Devuelve el valor como texto
+                return celda.getStringCellValue().trim(); 
             case NUMERIC:
                 if (DateUtil.isCellDateFormatted(celda)) {
                     // Si es una fecha, formatearla como texto
@@ -614,7 +610,7 @@ public class ExcelToDatabase {
                     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
                     return dateFormat.format(fecha);
                 } else {
-                    // Si es un número, devolverlo como texto sin notación científica
+                    
                     return String.valueOf((long) celda.getNumericCellValue());
                 }
             case BOOLEAN:
@@ -626,10 +622,10 @@ public class ExcelToDatabase {
     // Método auxiliar para obtener el valor de una celda como número
     private static Double obtenerValorNumerico(Cell celda) {
         if (celda == null || celda.getCellType() == CellType.BLANK) {
-            return null; // Devuelve null si la celda está vacía o es nula
+            return null; 
         }
         if (celda.getCellType() == CellType.NUMERIC) {
-            return celda.getNumericCellValue(); // Devuelve el valor numérico
+            return celda.getNumericCellValue(); 
         }
         return null; // Devuelve null para otros tipos no manejados
     }
